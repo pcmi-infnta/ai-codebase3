39eb1b8d8c5c734b70a77b4fa7c8dfeddbe09399
ejjays <christsonalloso021@gmail.com>
2025-04-24 13:26:57 +0800

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
@@ -21,6 +22,6 @@ tailwind.config = {
     },
   },
   plugins: [
-    tailwindTypography
+    require('@tailwindcss/typography')
   ],
 }
